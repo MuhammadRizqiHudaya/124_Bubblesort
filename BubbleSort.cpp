a05000624d4c8a60d84de96a83613f9b8b940b3b
HudayaW <hudayamuhammadrizqi@gmail.com>
2025-03-13 17:14:17 +0700

    Add Mmembuat bubble sort

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,55 +1,84 @@
 #include <iostream>
+#include <string>
 using namespace std;
-int a[15];		
-int n;			
 
-void input() {		
-	while (true) {	
-		cout << "Masukan banyaknya elemen pada array : "; 
-		cin >> n;	
-		if (n <= 15)	
-			break;	
-		else {		
-			cout << "\nArray dapat mempunyai maksimal 15 elemen. \n"; 
-		}
-	}
-	cout << endl;	
-	cout << "====================" << endl; 
-	cout << "Masukan Data" << endl; 
-	cout << "====================" << endl; 
+int arr[15];                
+int n;                      
 
-	for (int i = 0; i < n; i++) { 
-		cout << "Data ke-" << (i + 1) << ": ";	
-		cin >> a[i];		
-	}
+void input() {              
+    while(true) {
+        cout << "Masukkan jumlah data : ";  
+        cin >> n;           
+        if (n <= 15)        
+            break;          
+        else{
+            cout << "\nArray dapat mempunyai maksimal 15 data\n" << endl;
+        }
+    }
+
+
+cout << endl;
+cout << "=====================" << endl;
+cout << "Masukkan data" << endl;
+cout << "=====================" << endl;
+
+for (int i = 0; i <n; i++){                         
+    cout << "Data ke-" << (i+1) << ";";
+    cin >> arr[i];
 }
 
-void BubbleSortArray() {		
-	for (int i = 1; i < n; i++) {		
-		for (int j = 0; j < n - i; j++) {		
-			if (a[j] > a[j + 1]) {		
-				int temp = a[j];		
-				a[j] = a[j + 1];		
-				a[j + 1] = temp;		
-			}
-		}
-	}
 }
 
-void display() {		
-	cout << endl;		
-	cout << "=================================" << endl;		
-	cout << "Data Setelah Dirutkan" << endl;		
-	cout << "=================================" << endl;		
-	for (int j = 0; j < n; j++) {		
-		cout << a[j] << endl;		
-	}
-	cout << endl;		
+void bubbleSortArray(){
+    int pass = 1;                                   
+
+    do 
+    {
+        for (int j = 0; j <= n -1-pass;j++){        
+
+            if (arr[j] > arr[j + 1] )               
+            {
+                int temp;
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] + temp;
+            }  
+
+        }
+
+        pass = pass + 1;                            
+
+    }while (pass <= n - 1);                         
 }
 
-int main() {		
-	input();		
-	BubbleSortArray();		
-	display();
-	return 0;
+void display (){
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Data Setelah Diurutkan" << endl;
+    cout << "=================================" << endl;
+    cout << endl;
+    for (int j = 0; j < n; j++) {
+        cout << arr[j];                             
+        if (j < n - 1) {
+            cout << "-->";
+        }
+    }
+    cout << endl;
+    cout << endl;
+
+    cout << "Jumlah pass = " << endl;               
+    cout << endl;
+    cout << endl;
+
+}
+
+int main(){
+
+    input();
+
+    bubbleSortArray();
+    display();
+
+    system("pause");
+    return 0;
 }
